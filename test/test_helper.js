e1db5966d2d6c5a35df16641e767f7756ddb0964
Paul WB <higgsbison@gmail.com>
2017-01-20 20:01:53 -0800

    add jquery and wrap $ to use our jsdom window

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,9 +1,11 @@
 import jsdom from 'jsdom';
+import _$ from 'jquery';
 
 // set up test environment to run like a browser in the command line
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
 global.window = global.document.defaultView;
 
+const $ = _$(global.window);
 
 // build render component helper that should render a given react class
 
