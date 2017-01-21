c9101f4df7f42e925b3564f020aef3fa3935dec6
Paul WB <higgsbison@gmail.com>
2017-01-20 20:11:59 -0800

    add render component method to test helper

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,5 +1,7 @@
+import ReactDOM from 'react-dom';
 import jsdom from 'jsdom';
 import _$ from 'jquery';
+import TestUtils from 'react-addons-test-utils';
 
 // set up test environment to run like a browser in the command line
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
@@ -8,7 +10,10 @@ global.window = global.document.defaultView;
 const $ = _$(global.window);
 
 // build render component helper that should render a given react class
-
+export function renderComponent(ComponentClass) {
+  const componentInstance = TestUtils.renderIntoDocument(<ComponentClass />);
+  return $(ReactDOM.findDOMNode(componentInstance)); // produces HTML
+}
 
 // build helper for simulating events
 
